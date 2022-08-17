82e0c3432d642c2e64dc2ff4a0656c4485a89f48
Clement Denis <le.mikmac@gmail.com>
2022-08-17 20:59:38 +0100

    feat(env): auto reload env

--- env.js
+++ env.js
@@ -1,8 +1,16 @@
-export const isDev = process.env.NODE_ENV === 'development'
-export const DISCORD_SECRET = process.env.JANUS_SECRET
-export const CROISSANT_SECRET = process.env.CROISSANT_SECRET
-export const STRIPE_SIGNATURE = process.env.STRIPE_SIGNATURE
-export const STRIPE_SECRET = process.env.STRIPE_SECRET
-export const BOT_TOKEN = process.env.JANUS_TOKEN
-export const port = Number(process.env.PORT) || 8088
-export const DOMAIN = process.env.DOMAIN || 'admin.devazuka.com'
+import { readFileSync } from 'fs'
+
+const { env } = process
+try {
+  // try to import env.json
+  Object.assign(env, JSON.parse(readFileSync('../env.json', 'utf8')))
+} catch {}
+
+export const isDev = env.NODE_ENV === 'development'
+export const DISCORD_SECRET = env.JANUS_SECRET
+export const CROISSANT_SECRET = env.CROISSANT_SECRET
+export const STRIPE_SIGNATURE = env.STRIPE_SIGNATURE
+export const STRIPE_SECRET = env.STRIPE_SECRET
+export const BOT_TOKEN = env.JANUS_TOKEN
+export const port = Number(env.PORT) || 8088
+export const DOMAIN = env.DOMAIN || 'admin.devazuka.com'
